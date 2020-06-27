9cda45f9ef19c8e5ea52cb4d97d49a82ee71332a
rozina <raniyahnoor9988@gmail.com>
2020-06-27 19:15:24 +0500

    task done

---
+++ app.js
@@ -0,0 +1,13 @@
+function insertNum(num){
+    var result = document.getElementById("result");
+    result.value += num;
+}
+function clearNum(){
+    var result =document.getElementById("result")
+    result.value = ""
+}
+
+function getResult(){
+    var result = document.getElementById("result");
+    result.value = eval(result.value)
+}
